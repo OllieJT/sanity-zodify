c17afb11d406f19407f886c2a6a67c6a51a31f7b
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-09-25 00:38:57 +0100

    adds utility models

---
+++ src/models/sanity-models.ts
@@ -0,0 +1,88 @@
+import { z } from 'zod';
+import { stringDate } from './utility-models';
+
+export const SanityBoolean = z.boolean();
+export const SanityDate = stringDate;
+export const SanityDateTime = stringDate;
+export const SanityNumber = z.number();
+export const SanityString = z.string();
+export const SanityText = z.string();
+export const SanityUrl = z
+	.string()
+	.url()
+	.transform((value) => new URL(value));
+
+export const SanityReference = z.object({ _type: z.literal('reference'), _ref: z.string() });
+export const SanityDocument = z.object({
+	_id: z.string(),
+	_rev: z.string(),
+	_type: z.string(),
+	_createdAt: SanityDateTime,
+	_updatedAt: SanityDateTime,
+});
+
+export const SanitySlug = z.object({ _type: z.literal('slug'), current: z.string() });
+export const SanityGeopoint = z.object({
+	_type: z.literal('geopoint'),
+	alt: z.number(),
+	lat: z.number(),
+	lng: z.number(),
+});
+
+export const SanityImage = z.object({ _type: z.literal('image'), asset: SanityReference });
+const SanityImageAssetMetadataPalette = z.object({
+	_type: z.literal('sanity.imagePaletteSwatch'),
+	background: z.string(),
+	foreground: z.string(),
+	population: z.number(),
+	title: z.string(),
+});
+export const SanityImageAsset = SanityDocument.extend({
+	_type: z.literal('sanity.imageAsset'),
+	assetId: z.string(),
+	extension: z.string(),
+	mimeType: z.string(),
+	originalFilename: z.string(),
+	path: z.string(),
+	sha1hash: z.string(),
+	size: z.number(),
+	uploadId: z.string(),
+	url: z.string().url(),
+	metadata: z.object({
+		_type: z.literal('sanity.imageMetadata'),
+		blurHash: z.string(),
+		hasAlpha: z.boolean(),
+		isOpaque: z.boolean(),
+		lqip: z.string(),
+		dimensions: z.object({
+			_type: z.literal('sanity.imageDimensions'),
+			aspectRatio: z.number(),
+			height: z.number(),
+			width: z.number(),
+		}),
+		palette: z.object({
+			_type: z.literal('sanity.imagePalette'),
+			darkMuted: SanityImageAssetMetadataPalette,
+			darkVibrant: SanityImageAssetMetadataPalette,
+			dominant: SanityImageAssetMetadataPalette,
+			lightMuted: SanityImageAssetMetadataPalette,
+			lightVibrant: SanityImageAssetMetadataPalette,
+			muted: SanityImageAssetMetadataPalette,
+			vibrant: SanityImageAssetMetadataPalette,
+		}),
+	}),
+});
+
+export const SanityFile = z.object({ _type: z.literal('file'), asset: SanityReference });
+export const SanityFileAsset = SanityDocument.extend({
+	_type: z.literal('sanity.fileAsset'),
+	assetId: z.string(),
+	extension: z.string(),
+	mimeType: z.string(),
+	originalFilename: z.string(),
+	path: z.string(),
+	sha1hash: z.string(),
+	size: z.number(),
+	uploadId: z.string(),
+	url: z.string().url(),
+});
---
+++ src/models/utility-models.ts
@@ -0,0 +1,3 @@
+import { z } from 'zod';
+
+export const stringDate = z.string().transform((v) => (v?.length ? new Date(v) : undefined));
